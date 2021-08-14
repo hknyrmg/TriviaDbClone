3543b5d036dc38aae77439e40dd885d7d3ecee1a
hakanyarimaga1202 <hakanyarimaga1202@hotmail.com>
2021-08-14 17:41:47 +0300

    .

--- TriviaDbClone/Utils/RapidApiConstants.cs
+++ TriviaDbClone/Utils/RapidApiConstants.cs
@@ -7,7 +7,7 @@ namespace TriviaDbClone.Utils
 {
     public class RapidApiConstants
     {
-        public static string RapidApiKey = "44f2c854c9msh70052d5b0318de0p115f87jsna34187fcf3fb";
+        public static string RapidApiKey = "******";
         public static string RapidApiHost = "unogsng.p.rapidapi.com";
         public static string RapidApiBaseUri = "https://opentdb.com/api.php?amount=50";
 
